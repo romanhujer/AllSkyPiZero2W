5cf563e755f4e375a94dab28c056fcb8fd37ae45
Roman Hujer <roman@hujer.net>
2021-11-22 01:25:55 +0100

    GIPO script

---
+++ DH_5m.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+#   fan_ON.py 
+#
+#   Version 1.0
+#
+#   Copyright (c) 2020 Roman Hujer   http://hujer.net
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,ss
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  Description:
+#
+#
+
+
+
+import os 
+import logging
+import time 
+import argparse 
+import RPi.GPIO as GPIO
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(13, GPIO.OUT)
+GPIO.output(13, GPIO.HIGH)
+
+time.sleep(300)
+GPIO.output(13, GPIO.LOW)
+
